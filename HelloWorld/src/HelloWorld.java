136259893a8a777e944f0f00642e05c195faef7a
dhuntley1023 <dhuntley1023@hotmail.com>
2013-12-31 13:27:35 -0800

    Established HelloWorld project

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,40 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int i;
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.append("H").append("i");
+		System.out.println(" There");
+		System.out.println("Oops!");
+//		System.out.println(args[0]);
+		
+		i=0;
+		while (i < args.length) {
+			System.out.println(args[i]);
+			i++;
+		}
+		
+		for (int i1=0; i1 < args.length; i1++) {
+			System.out.println(args[i1]);
+		}
+
+		ArgPrinter ap = new ArgPrinter().printargs(args);
+		
+		ap.printargs(args).printargs(args);
+	}
+
+}
+
+
+class ArgPrinter {
+	public ArgPrinter printargs(String[] args) {
+		System.out.println("Inside printargs");
+		for (int i1=0; i1 < args.length; i1++) {
+			System.out.println(args[i1]);
+		}
+		return this;
+	}
+}
